c41c3acd77823ebd7c68a9c49fa492428c31b56b
DAM <egrao@cifpfbmoll.eu>
2022-01-31 14:55:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/listviews/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.listviews;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends ListActivity {
+
+    private TextView selection;
+    private static final String[] items={"lorem", "ipsum", "dolor",
+            "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel", "erat", "placerat", "ante",
+            "porttitor", "sodales", "pellentesque", "augue", "purus"};
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this,
+                R.layout.row, R.id.textViewRow,
+                items));
+        selection=(TextView)findViewById(R.id.selection);
+    }
+
+    public void onListItemClick(ListView parent, View v,
+                                int position, long id) {
+        selection.setText(items[position]);
+    }
+}
